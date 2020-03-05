3af6c3ef0a2a9804bc0f3e542f90359f3da0197a
oliviernda <oliviernda@gmail.com>
2020-03-05 01:20:18 +0100

    ajoute des outlet et actions

--- InstaGrid/Controller/ViewController.swift
+++ InstaGrid/Controller/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var layoutButtons: [UIButton]!
+    @IBOutlet var gridButtons: [UIButton]!
+    @IBOutlet weak var topLeftgridButton: UIButton!
+    @IBOutlet weak var bottomLeftGridButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func layoutButtonTapped(_ sender: UIButton) {
+        layoutButtons.forEach { $0.isSelected = false}
+        
+        // for button in layoutButtons {
+        //   button.isSelected = false
+        // }
+        sender.isSelected = true
+        
+        switch sender.tag {
+        case 0:
+            topLeftgridButton.isHidden = true
+            bottomLeftGridButton.isHidden = false
+        case 1:
+            topLeftgridButton.isHidden = false
+            bottomLeftGridButton.isHidden = true
+        case 2 :
+            topLeftgridButton.isHidden = false
+            bottomLeftGridButton.isHidden = false
+        default:
+            break
+        }
+    }
 
 }
 
