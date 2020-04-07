6fa93fff83665a0dfffef76fd2f50703b00ce733
oliviernda <oliviernda@gmail.com>
2020-04-07 18:19:07 +0200

    Adding code to pick Image

--- InstaGrid/Controller/ViewController.swift
+++ InstaGrid/Controller/ViewController.swift
@@ -14,17 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet var gridButtons: [UIButton]!
     @IBOutlet weak var topLeftgridButton: UIButton!
     @IBOutlet weak var bottomLeftGridButton: UIButton!
+    
+    
+    let imagePickerConroler = UIImagePickerController()
+    var tag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePickerConroler.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func layoutButtonTapped(_ sender: UIButton) {
         layoutButtons.forEach { $0.isSelected = false}
+       
         
-        // for button in layoutButtons {
-        //   button.isSelected = false
-        // }
         sender.isSelected = true
         
         switch sender.tag {
@@ -41,6 +47,21 @@ class ViewController: UIViewController {
             break
         }
     }
-
+    @IBAction func imagePikerAction(_ sender: UIButton) {
+        tag = sender.tag
+        present(imagePickerConroler, animated: true)
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let imageSelected = info [.originalImage] as? UIImage else { return }
+        gridButtons [tag].setImage(imageSelected, for: .normal)
+        gridButtons [tag].layoutIfNeeded()
+        gridButtons [tag].subviews.first?.contentMode = .scaleAspectFill
+        print (imageSelected)
+        picker.dismiss(animated: true)
+    }
+    
+}
